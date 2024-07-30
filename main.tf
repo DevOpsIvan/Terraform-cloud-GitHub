b5495c509d40aa13bce91bc5a22ba08210b2c87d
DevOpsIvan <162887183+DevOpsIvan@users.noreply.github.com>
2024-07-30 09:22:35 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-    required_version = "~> 1.3.6" // added latest version
+    required_version = "~> 1.9.2" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.49.0"
+      version = "~> 5.60.0"
      }
     }
    }
